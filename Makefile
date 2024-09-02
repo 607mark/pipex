9598b82b88ca9befd7d5d142365ee7c048c833ac
607mark <40577872+607mark@users.noreply.github.com>
2024-09-02 17:02:46 +0300

    make

---
+++ Makefile
@@ -0,0 +1,47 @@
+NAME = pipex
+
+SRCS =  pipex.c
+
+OBJS = $(SRCS:.c=.o)
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+
+LIBFT_DIR = ./libft
+FT_PRINTF_DIR = $(LIBFT_DIR)/ft_printf
+LIBFT = -L$(LIBFT_DIR) -lft
+
+
+# Default target
+all: $(NAME)
+
+# Build the executable
+$(NAME): $(OBJS) $(LIBFT_DIR)/libft.a
+	$(CC) $(OBJS) $(LIBFT) -o $(NAME)
+
+# Compile libft and ft_printf
+$(LIBFT_DIR)/libft.a:
+	@$(MAKE) -C $(FT_PRINTF_DIR) all
+	@$(MAKE) -C $(LIBFT_DIR) all
+	@touch $(LIBFT_DIR)/libft.a
+
+# Compile object files
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# Clean object files
+clean:
+	rm -f $(OBJS)
+	@$(MAKE) -C $(LIBFT_DIR) clean
+	@$(MAKE) -C $(FT_PRINTF_DIR) clean
+
+# Clean all, including the executable
+fclean: clean
+	rm -f $(NAME)
+	@$(MAKE) -C $(LIBFT_DIR) fclean
+	@$(MAKE) -C $(FT_PRINTF_DIR) fclean
+
+# Rebuild the project
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ libft/ft_lstadd_back_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshabano <mshabano@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 22:02:46 by mshabano          #+#    #+#             */
+/*   Updated: 2024/08/17 14:25:39 by mshabano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	last = ft_lstlast(*lst);
+	if (!last)
+	{
+		*lst = new;
+		return ;
+	}
+	last->next = new;
+	return ;
+}
