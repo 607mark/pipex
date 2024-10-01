5f1c2b019ca45267a3985b9a44c4c3079deed4ba
607mark <40577872+607mark@users.noreply.github.com>
2024-10-01 12:52:39 +0300

    tiny fixes

--- main.c
+++ main.c
@@ -32,28 +32,22 @@ void	exit_error(char *s, int n, t_pipex *p)
 
 void	parent_process(t_pipex *p)
 {
-	p->outfile_fd = open(p->outfile_name, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-	if (p->outfile_fd == -1)
-		exit_error(p->outfile_name, 1, p);
+	fd_close(&p->pipe_fd[1]);
 	if (dup2(p->outfile_fd, 1) == -1)
 		exit_error("dup2()", 1, p);
 	if (dup2(p->pipe_fd[0], 0) == -1)
 		exit_error("dup2()", 1, p);
-	fd_close(&p->pipe_fd[1]);
 	fd_close(&p->outfile_fd);
 	execute_cmd(p->cmd2, p);
 }
 
 void	child_process(t_pipex *p)
 {
-	p->infile_fd = open(p->infile_name, O_RDWR);
-	if (p->infile_fd < 0)
-		exit_error(p->infile_name, 1, p);
+	fd_close(&p->pipe_fd[0]);
 	if (dup2(p->infile_fd, 0) == -1)
 		exit_error("dup2()", 1, p);
 	if (dup2(p->pipe_fd[1], 1) == -1)
 		exit_error("dup2()", 1, p);
-	fd_close(&p->pipe_fd[0]);
 	fd_close(&p->infile_fd);
 	execute_cmd(p->cmd1, p);
 }
@@ -72,6 +66,12 @@ void	pipex_init(t_pipex *p, char **av, char **env)
 	pipex_getenv_path(p);
 	if (p->path == NULL)
 		exit_error("PATH not found", 0, p);
+	p->infile_fd = open(p->infile_name, O_RDWR);
+	if (p->infile_fd < 0)
+		exit_error(p->infile_name, 1, p);
+	p->outfile_fd = open(p->outfile_name, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (p->outfile_fd == -1)
+		exit_error(p->outfile_name, 1, p);
 }
 
 int	main(int ac, char **av, char **env)
